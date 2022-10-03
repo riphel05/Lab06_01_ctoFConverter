757a3548b72780006c02009b54bf67d0318d50aa
riphel05 <phelpsre@mail.uc.edu>
2022-10-02 22:45:01 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+<<<<<<< HEAD
+=======
+        String trash = "";
+>>>>>>> 036817b (Initial commit)
         double tempF = 0;
         double tempC = 0;
 
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter the temp in Celsius: ");
+<<<<<<< HEAD
         tempC = in.nextDouble();
 
         tempF = (tempC * 1.8) + 32;
 
         System.out.println("With the temp of " + tempC + "Celsius, the temp in Fahrenheit is " + tempF );
+=======
+        if(in.hasNextDouble()) {
+            tempC = in.nextDouble();
+        }
+        else
+        {
+            System.out.println("You said the temp was " + tempC + trash );
+            System.out.println("Please enter a correct temp.");
+            System.exit(0);
+        }
+        tempF = (tempC * 1.8) + 32;
+
+        System.out.println("With the temp of " + tempC + "Celsius, the temp in Fahrenheit is " + tempF );
+
+>>>>>>> 036817b (Initial commit)
     }
 }
