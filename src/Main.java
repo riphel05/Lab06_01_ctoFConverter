a30979aac54577f7cc91b18d062388c595d9b00d
riphel05 <phelpsre@mail.uc.edu>
2022-10-17 01:11:34 -0400

    second time

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-=======
         String trash = "";
->>>>>>> 036817b (Initial commit)
-        double tempF = 0;
-        double tempC = 0;
-
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter the temp in Celsius: ");
-<<<<<<< HEAD
-        tempC = in.nextDouble();
-
-        tempF = (tempC * 1.8) + 32;
-
-        System.out.println("With the temp of " + tempC + "Celsius, the temp in Fahrenheit is " + tempF );
-=======
-        if(in.hasNextDouble()) {
-            tempC = in.nextDouble();
-        }
-        else
+        double tempF;
+        double tempC;
+        boolean done = false;
+        do
         {
-            System.out.println("You said the temp was " + tempC + trash );
-            System.out.println("Please enter a correct temp.");
-            System.exit(0);
-        }
-        tempF = (tempC * 1.8) + 32;
 
-        System.out.println("With the temp of " + tempC + "Celsius, the temp in Fahrenheit is " + tempF );
+            Scanner in = new Scanner(System.in);
 
->>>>>>> 036817b (Initial commit)
+            System.out.print("Enter the temp in Celsius: ");
+            tempC = in.nextDouble();
+
+            tempF = (tempC * 1.8) + 32;
+
+            System.out.println("With the temp of " + tempC + "Celsius, the temp in Fahrenheit is " + tempF);
+            if(in.hasNextDouble())
+            {
+                tempC = in.nextDouble();
+                in.nextLine();
+                if(tempC >= 0 || tempC <= 100)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("You must enter the a value from the freezing point (0) to the boiling point (100) " + tempC);
+                }
+            }
+            else
+            {
+                System.out.println("You said the temp was " + tempC + trash);
+                System.out.println("Please enter a correct temp.");
+                System.exit(0);
+            }
+            tempF = (tempC * 1.8) + 32;
+
+            System.out.println("With the temp of " + tempC + "Celsius, the temp in Fahrenheit is " + tempF);
+        } while(!done);
     }
 }
